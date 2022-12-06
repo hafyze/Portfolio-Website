ce274fce459ba40b0ee0682654e69fd24352bea6
hafyze <hafizmatqwer@gmail.com>
2022-12-06 15:43:34 +0800

    Update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+
+scroll();
+
+function scroll(){
+    var x = document.getElementById("myTopnav");
+         if (x.className === "topnav") {
+            x.className += " responsiveNav";
+        } else {
+            x.className = "topnav";
+        }
+}
